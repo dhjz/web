37f3c328cec62f4c0e528883e78a3508dd5c3585
dinghj3 <dinghj3@asiainfo.com>
2024-04-28 11:41:18 +0800

    cloudflare9

--- functions/cloud/proxy.js
+++ functions/cloud/proxy.js
@@ -6,19 +6,23 @@ export const onRequest = async ({ request }) => {
   let { url, type = 'text/plain;charset=utf-8' } = params
   let result = ''
   if (url) {
-    const res = await axios({
-      method: 'get',
-      url: url,
-      responseType: 'text',
-      timeout: 30000,
-    })
-    result = res.data || ''
+    try {
+      const res = await axios({
+        method: 'get',
+        url: url,
+        responseType: 'text',
+        timeout: 30000,
+      })
+      result = res.data || ''
+    } catch(e) {
+      result = '400 axios 请求错误'
+    }
     // type = res.headers['content-type']
   }
 
   console.log(result.substring(0, 50), type);
 
-  const response = new Response(result.substring(0, 50) || ('暂未获取参数或者请求结果, ' + request.url + ' ||| ' + JSON.stringify(params)));
+  const response = new Response(result || ('暂未获取参数或者请求结果, ' + request.url + ' ||| ' + JSON.stringify(params)));
   // const response = new Response(JSON.stringify(params) + url + (typeof axios));
   response.headers.set('Content-Type', type)
   return response;
