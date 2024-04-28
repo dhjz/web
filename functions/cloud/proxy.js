19446821b8b170451c6f594b0dcaca8897037b17
dinghj3 <dinghj3@asiainfo.com>
2024-04-28 11:08:49 +0800

    cloudflare5

--- functions/cloud/proxy.js
+++ functions/cloud/proxy.js
@@ -4,21 +4,22 @@ export const onRequest = async (ctx) => {
   const { request, params } = ctx
 
   let { url, type = 'text/plain' } = params
-  let result = ''
-  if (url) {
-    const res = await axios({
-      method: 'get',
-      url: url,
-      responseType: 'text',
-      timeout: 30000,
-    })
-    result = res.data
-    type = res.headers['content-type']
-  }
+  // let result = ''
+  // if (url) {
+  //   const res = await axios({
+  //     method: 'get',
+  //     url: url,
+  //     responseType: 'text',
+  //     timeout: 30000,
+  //   })
+  //   result = res.data
+  //   type = res.headers['content-type']
+  // }
 
-  console.log(result.substring(0, 50), type);
+  // console.log(result.substring(0, 50), type);
 
-  const response = new Response(result);
+  // const response = new Response(result || JSON.stringify(params));
+  const response = new Response(JSON.stringify(params) + url + (typeof axios));
   response.headers.set('Content-Type', type)
   return response;
 };
