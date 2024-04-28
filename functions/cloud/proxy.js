4767608b11e3f43d92795830c5cb0d6c7e770930
dinghj3 <dinghj3@asiainfo.com>
2024-04-28 11:34:34 +0800

    cloudflare9

--- functions/cloud/proxy.js
+++ functions/cloud/proxy.js
@@ -4,17 +4,17 @@ export const onRequest = async ({ request }) => {
   const params = Object.fromEntries(new URL(request.url).searchParams);
 
   let { url, type = 'text/plain;charset=utf-8' } = params
-  let result = ''
-  if (url) {
-    const res = await axios({
-      method: 'get',
-      url: url,
-      responseType: 'text',
-      timeout: 30000,
-    })
-    result = res.data
-    type = res.headers['content-type']
-  }
+  // let result = ''
+  // if (url) {
+  //   const res = await axios({
+  //     method: 'get',
+  //     url: url,
+  //     responseType: 'text',
+  //     timeout: 30000,
+  //   })
+  //   result = res.data
+  //   // type = res.headers['content-type']
+  // }
 
   console.log(result.substring(0, 50), type);
 
