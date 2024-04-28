6b9be0aa610a6abc8db0307eddcc079756fce77a
dinghj3 <dinghj3@asiainfo.com>
2024-04-28 11:03:58 +0800

    cloudflare4

--- functions/cloud/ip.js
+++ functions/cloud/ip.js
@@ -1,6 +1,6 @@
 export const onRequest = async (ctx) => {
   const { request, params } = ctx
-  const result = request.headers.get('client-ip') || request.headers.get('x-nf-client-connection-ip') || request.headers.get('x-forwarded-for')
+  const result = request.headers.get('Client-Cp') || request.headers.get('x-nf-client-connection-ip') || request.headers.get('X-Forwarded-For') || request.headers.get('CF-Connecting-IP')
   console.log(result, request.headers);
-  return new Response(result || ('暂未到获取ip' + JSON.stringify(request.headers)));
+  return new Response(result || ('暂未到获取ip' + JSON.stringify(request)));
 };
