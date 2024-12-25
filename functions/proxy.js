8e7525293d622f947731c5ac8bd96d53e85c47b4
dinghj3 <dinghj3@asiainfo.com>
2024-12-25 11:05:50 +0800

    代理

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,15 +1,21 @@
 export const onRequest = async ({ request }) => {
   const params = Object.fromEntries(new URL(request.url).searchParams);
 
-  let { url, type = 'text/plain;charset=utf-8' } = params
+  let { url, method = 'get', type = 'text/plain;charset=utf-8', host, referer, cookie, accept } = params
   let result = ''
   if (url) {
     try {
+      const headers = {
+        // 'Accept': 'text/plain; charset=utf-8',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+      }
+      if (host) headers['Host'] = host
+      if (referer) headers['Referer'] = referer
+      if (cookie) headers['Cookie'] = cookie
+      if (accept) headers['Accept'] = accept
       const res = await fetch(url, {
-        method: 'get',
-        headers: {
-          'Accept': 'text/plain; charset=utf-8'
-        }
+        method,
+        headers: headers
       })
       result = await res.text()
     } catch(e) {
