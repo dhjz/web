f49e68554e8f0cf361bf1e28f6c1942e2b312816
dhjz <909514844@qq.com>
2025-10-13 09:17:28 +0800

    1

--- utils/bat.js
+++ utils/bat.js
@@ -101,7 +101,7 @@ if "%choice%"=="3" (
 )
 if "%choice%"=="4" (
   :: 安装 rsrc:  go install github.com/akavel/rsrc@latest
-  :: 只在windows上生成图标 ico_windows_amd64.syso 
+  :: 只在windows上生成图标 ico_windows_amd64.syso 或者ico_windows.syso 
   cd service
   rsrc -ico="ico.ico" -o="ico_windows_amd64.syso"
   pause
