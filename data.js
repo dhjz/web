192773ae323207b826b313a86df082c548fa7b31
dhjz <909514844@qq.com>
2025-10-15 17:41:16 +0800

    1

--- data.js
+++ data.js
@@ -27,6 +27,7 @@ window.config = {
         { name: '字符压缩', short: '压缩', url: './pako/index.html', color: '#861683' },
         { name: '工具类', short: '工具类', url: './utils/lib.html', color: '#8B4513' },
         { name: 'JS库CDN', short: 'CDN', url: 'https://cdn.199311.xyz/', color: '#138b43' },
+        { name: 'MD文件预览', short: 'MD', url: './md/index.html', color: '#2655bbff' },
       ]
     },
     {
