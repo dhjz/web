3b3e9dd4e06bb046281e2ceaf14415bdbb074f5d
dhjz <35137457+dhjz@users.noreply.github.com>
2026-03-05 13:55:57 +0800

    update data.js

--- data.js
+++ data.js
@@ -51,7 +51,7 @@ window.config = {
         { name: 'AList', short: 'AList', url: 'http://home.199311.xyz:25244/', color: '#238FE7' },
         { name: '局域网唤醒', short: 'WOL', url: 'http://home.199311.xyz:46666/', color: '#0066cc' },
         { name: '音乐之家', short: '音乐', url: 'https://music.199311.xyz/', color: '#49ABFC' },
-        { name: '多线程下载', short: '下载', url: './down/index.html', color: '#56C4E3' },
+        { name: '内云服务', short: '云服务', url: 'http://home.199311.xyz:42220', color: '#56C4E3' },
         { name: 'SSL证书更新', short: 'SSL', url: 'http://home.199311.xyz:40055', color: '#35C0F4' },
       ]
     },
