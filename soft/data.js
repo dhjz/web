d1cd9fa110aafb36b62bdc81c836674549433b8e
dhjz <909514844@qq.com>
2026-03-17 16:49:46 +0800

    1

--- soft/data.js
+++ soft/data.js
@@ -2,4 +2,13 @@ var apps = [
   { name: '果核剥壳', url: 'https://www.ghxi.com/?s=%s' },
   { name: '枫音6B', url: 'https://www.fy6b.com/?s=%s' },
   { name: '六音软件', url: 'https://www.sixyin.com/?s=%s' },
+  { name: '克隆窝', url: 'https://www.uy5.net/?s=%s' },
+  { name: '辉哥博客', url: 'https://www.haah.net/?s=%s' },
+  { name: '绿软部落', url: 'https://www.ludown.com/?s=%s' },
+  { name: 'APP喵', url: 'https://www.appmiu.com/?s=%s&type=post' },
+  { name: '乐享', url: 'https://www.lxapk.com/?s=%s&type=post' },
+  { name: '大眼仔', url: 'https://www.dayanzai.me/?s=%s' },
+
+  { name: '软件No1', url: 'https://www.rjno1.com/?s=%s' },
+  { name: 'SUG软件', url: 'https://sug.xyz/rj/?s=%s&post_type=app' },
 ]
