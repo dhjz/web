c86fedb589af3b1f7b2ab6cbadb8bce263036c60
dhjz <909514844@qq.com>
2026-05-12 09:42:41 +0800

    svg

--- data.js
+++ data.js
@@ -68,7 +68,7 @@ window.config = {
         { name: '音频转换', short: '音频转', url: './audioc/index.html', color: '#DC143C' },
         { name: 'ICON转换', short: 'ICO', url: './ico/index.html', color: '#9370DB' },
         { name: '图片压缩', short: '压缩', url: 'https://tiny.199311.xyz/', color: '#ff3385' },
-        { name: 'SVG编辑', short: 'SVG', url: 'https://svg.199311.xyz/', color: '#D4237A' },
+        { name: 'SVG编辑', short: 'SVG', url: './svg/index.html', color: '#D4237A' },
         { name: '水印工具', short: '水印', url: './mark/index.html', color: '#111827' },
       ]
     },
